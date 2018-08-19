d8b3f81fb5df51e9ac9987de82ed4d7c2eb457ff
jackypy <jacqueline.parra7@gmail.com>
2018-08-19 10:46:27 -0300

    Ejercicio_04_Métodos

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,13 @@
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+
+def saludo(x)
+  if x == "hola"
+    p "hola mundo"
+  end
+end
+ saludo("hola") #el parametro (x) siempre tomara el valor "hola"
+ saludo("h")
+ saludo("gffytct")
+ saludo("hola")
