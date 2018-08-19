c6b5d87675608cf047fbab75fc314952b258491c
jackypy <jacqueline.parra7@gmail.com>
2018-08-19 11:26:19 -0300

    Ejercicio_06_Métodos

--- 1 metodos/ejercicio6.rb
+++ 1 metodos/ejercicio6.rb
@@ -7,12 +7,12 @@
 # *****
 
 def draw_line(size)
-  '*' * size
+   '*' * size
 end
 
 def draw_lines(size)
-  size.times { }
+  size.times { p draw_line(size)  }
 end
 
-draw_line 2
-draw_lines 10
+draw_line(2)
+draw_lines(10)
