0420957d3fe69309baf2a0623c9dd7694d71d410
jackypy <jacqueline.parra7@gmail.com>
2018-08-19 11:03:15 -0300

    Ejercicio_05_Métodos

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,12 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+
+def pares(x, y)
+  (x..y).each { |e|
+    if e.even?
+      p "#{e} par"
+    end
+  }
+end
+
+pares(2, 27)
